d4a945fbd98b356e481abe5f5f5c0d490c002718
Caaronbll <aaronbll46@gmail.com>
2024-04-02 19:17:32 -0500

    parsing data

--- data_base/transformer/transformer.py
+++ data_base/transformer/transformer.py
@@ -6,10 +6,19 @@ titles = []
 dates = []
 notes = []
 
+painting_indexs = []
+img_srcs = []
 seasons = []
-first_line =[]
+episodes =[]
+num_of_colors = []
+yt_srcs = []
+colors_list = []
+hex_list = []
+
+subject_lists = []
 
 
+# function 1 - gets 3 lists
 def parse_episode_data(file_name):
     with open(file_name, 'r') as file:
         # read each line
@@ -31,29 +40,77 @@ def parse_episode_data(file_name):
             notes.append(notes_str)
 
 
+# function 2 - gets 8 lists
 def parse_colors_data(file_name):
     with open(file_name, 'r') as file:
         # read each line
         for line in file:
             line = line.strip()
-            print(line)
+            painting_ndx_str, img_src_str, season_str, episode_str = "", "", "", ""
+            num_colors_str, yt_src_str, colors_str, hex_str = "", "", "", ""
 
+            painting_ndx_str, img_src_str = line.split(',')[1], line.split(',')[2]
+            season_str, episode_str = line.split(',')[4], line.split(',')[5]
+            num_colors_str, yt_src_str = line.split(',')[6], line.split(',')[7]
 
+            if len(line.split('"')) > 1:
+                colors_str = line.split('"')[1]
+                hex_str = line.split('"')[3]
 
+            painting_indexs.append(painting_ndx_str)
+            img_srcs.append(img_src_str)
+            seasons.append(season_str)
+            episodes.append(episode_str)
+            num_of_colors.append(num_colors_str)
+            yt_srcs.append(yt_src_str)
+            colors_list.append(colors_str)
+            hex_list.append(hex_str)
+         
+
+# function 3 - gets lists
+def parse_subject_data(file_name):
+    with open(file_name, 'r') as file:
+        sl =[]
+        mock_data = []
+        # read each line
+        for line in file:
+            line = line.strip()
+            
+            if line[0] == 'E':
+                sl = line.split(',')[2:69]
+            else:
+                sub_data = line.split(',')[2:69]
+                mock_data.append(sub_data)
+            
+        for i in mock_data:
+            mock_list = []
+            for j in range(len(sl)):
+                if i[j] == '1':
+                    mock_list.append(sl[j])
+            subject_lists.append(mock_list)
+
+        
+        
+# creating new file
+def createfile():
+    
+    with open('./data/new_data.csv', 'w') as new_file:
+        writer = csv.writer(new_file)
+        writer.writerow(['painting_index', 'img_src','season', 'episode', 'num_of_colors', 'yt_src', 'colors_list', 'hex_list','subject_list', 'notes'])
+        for i in range(len(painting_indexs)):
+            writer.writerow([painting_indexs[i], img_srcs[i], seasons[i], episodes[i], num_of_colors[i], yt_srcs[i], colors_list[i], hex_list[i], subject_lists[i], notes[i]]) 
+            
+
+# calling functions with apropiate files
 parse_episode_data('./data/episode_data.txt')
 parse_colors_data('./data/colors_data.csv')
-
-print()
-
-
-
-
-
-
-
-
-    
-        
-
-
-
+parse_subject_data('./data/subject_data.csv')
+painting_indexs = painting_indexs[1:]
+img_srcs = img_srcs[1:]
+seasons = seasons[1:]
+episodes = episodes[1:]
+num_of_colors = num_of_colors[1:]
+yt_srcs = yt_srcs[1:]
+colors_list = colors_list[1:]
+hex_list = hex_list[1:]
+createfile()
